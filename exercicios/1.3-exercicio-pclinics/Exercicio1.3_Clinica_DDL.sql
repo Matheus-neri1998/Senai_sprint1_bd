d8db4a1f1e3c0e3cf4dcc390e7e456f5b6361f6f
Matheus <mthneri@gmail.com>
2021-02-26 21:09:13 -0300

    Exercícios 1.3 e 1.4 usando conceito de DDL do banco de dados

---
+++ exercicios/1.3-exercicio-pclinics/Exercicio1.3_Clinica_DDL.sql
@@ -0,0 +1,69 @@
+/*
+	Exercício 1.3 - Clínica 
+*/
+
+CREATE DATABASE Clinica
+
+USE Clinica
+
+-- Tabela 1 Clinicas --
+CREATE TABLE Clinicas
+(
+	IdClinica INT PRIMARY KEY
+	,RazaoSocial VARCHAR (100) NOT NULL
+	,CNPJ VARCHAR (100) NOT NULL
+	,Endereco VARCHAR (200) NOT NULL
+);
+
+-- Tabela 2 - Tipos de PETs --
+CREATE TABLE TiposPets
+(
+	IdTipoPet INT PRIMARY KEY
+	,Descricao VARCHAR (200) NOT NULL
+);
+
+-- Tabela 3 - Racas --
+CREATE TABLE Racas
+(
+	IdRaca INT PRIMARY KEY
+	,Descricao VARCHAR (200) NOT NULL
+	,IdTipoPet INT FOREIGN KEY REFERENCES TiposPets (IdTipoPet)
+);
+
+-- Tabela 4 - Donos --
+CREATE TABLE Donos
+(
+	IdDono INT PRIMARY KEY
+	,Nome VARCHAR (200) NOT NULL
+);
+
+-- Tabela 5 - Pets --
+CREATE TABLE Pets
+(
+	IdPet INT PRIMARY KEY
+	,Nome VARCHAR (100) NOT NULL
+	,DataNascimento VARCHAR (50) NOT NULL
+	,IdRaca INT FOREIGN KEY REFERENCES Racas (IdRaca)
+	,IdDono INT FOREIGN KEY REFERENCES Donos (IdDono)
+);
+
+-- Tabela 6 - Veterinários --
+CREATE TABLE Veterinarios
+(
+	IdVeterinario INT PRIMARY KEY
+	,Nome VARCHAR (100) NOT NULL
+	,CRMV VARCHAR (100) NOT NULL
+	,IdClinica INT FOREIGN KEY REFERENCES Clinicas (IdClinica)
+
+);
+
+-- Tabela 7 - Atendimentos --
+CREATE TABLE Atendimentos
+(
+	IdAtendimento INT PRIMARY KEY 
+	,Descricao VARCHAR (200) NOT NULL
+	,DataAtendimento VARCHAR (100) NOT NULL
+	,IdVeterinario INT FOREIGN KEY REFERENCES Veterinarios (IdVeterinario)
+	,IdPet INT FOREIGN KEY REFERENCES Pets (IdPet)
+
+);
---
+++ exercicios/1.4-exercicio-optus/Exercicio1.4_Ramo_Musical_DDL.sql
@@ -0,0 +1,51 @@
+/*
+	Exercício 1.4 - Ramo Musical
+*/
+CREATE DATABASE Optus
+
+USE Optus
+
+-- Tabela 1 - Artistas --
+CREATE TABLE Artistas
+(
+	IdArtista INT PRIMARY KEY
+	,Nome VARCHAR (200) NOT NULL
+);
+
+-- Tabela 2 - Estilos --
+CREATE TABLE Estilos
+(
+	IdEstilo INT PRIMARY KEY
+	,Nome VARCHAR (200) NOT NULL
+);
+
+-- Tabela 3 - Álbuns de Estilos --
+CREATE TABLE AlbunsEstilos
+(
+	IdAlbumEstilo INT PRIMARY KEY
+	,IdEstilo INT FOREIGN KEY REFERENCES Estilos (IdEstilo)
+);
+
+-- Tabela 4 - Álbuns --
+CREATE TABLE Albuns
+(
+	IdAlbum INT PRIMARY KEY
+	,Titulo VARCHAR (250) NOT NULL
+	,DataNascimento VARCHAR (100) NOT NULL
+	,Localizacao VARCHAR (100) NOT NULL
+	,QtdMinutos VARCHAR (250) NOT NULL
+	,Ativo VARCHAR (100) NOT NULL
+	,IdArtista INT FOREIGN KEY REFERENCES Artistas (IdArtista)
+);
+
+-- Tabela 5 - Usuários --
+CREATE TABLE Usuarios
+(
+	IdUsuario INT PRIMARY KEY 
+	,Nome VARCHAR (200) NOT NULL
+	,Email VARCHAR (200) NOT NULL
+	,Senha VARCHAR (250) NOT NULL
+	,Permissao VARCHAR (100) NOT NULL
+
+);
+
